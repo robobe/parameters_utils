2f30b8c7e61510dcb8b051dfcd47a062c5f4da7b
robo2020 <robo2020@gmail.com>
2025-03-02 09:06:02 +0000

    integration with node,
    support load and save

--- src/parameters_manager_ex/parameters_manager_ex/__init__.py
+++ src/parameters_manager_ex/parameters_manager_ex/__init__.py
@@ -4,12 +4,29 @@
 from rclpy.node import Node
 from std_srvs.srv import Trigger
 import yaml
+import os
 
-TOPIC = "save_tracking_parameters"
+TOPIC = "dump"
 
 class ParameterManagerEx():
     def __init__(self, node: Node):
         self.node = node
+        self._data = None
+        self.location = self.node.get_parameter("param_yaml_full_path").value
+        self._load_parameters()
+        
+        self._init_service()
+
+
+    def _load_parameters(self):
+        if not os.path.exists(self.location):
+            self.node.get_logger().error(f"Parameter file not found: {self.location}")
+            return
+        
+        with open(self.location, "r") as file:
+            self._data = yaml.safe_load(file)
+        self.append_yaml_to_node(self._data)
+
 
     def _init_service(self):
         self.dump_srv = self.node.create_service(Trigger, 
@@ -17,7 +34,12 @@ class ParameterManagerEx():
                                 self.save_tracking_parameters)
         
     def _create_node_handler(self, key, value):
-        self.node.declare_parameter(key, value)
+        self.node.get_logger().error(f"--- {key} -- {value}")
+        if self.node.has_parameter(key):
+            #TODO: check if there beater way
+            self.node.get_parameter(key)._value = value
+        else:
+            self.node.declare_parameter(key, value)
         
     def traverse_yaml(self, data, parent_key="", handler=None):
         if isinstance(data, dict):
@@ -36,10 +58,11 @@ class ParameterManagerEx():
         self.traverse_yaml(data, handler=self._create_node_handler)
 
     def save_tracking_parameters(self, request: Trigger.Request, response: Trigger.Response):
+        self.get_tracking_parameters(self._data)
 
-
-        with open("/tmp/output.yaml", "w") as file:
-            yaml.dump(temp_yaml_file, file, default_flow_style=False, sort_keys=False, indent=4)
+        # TODO: add exception to logger
+        with open(self.location, "w") as file:
+            yaml.dump(self._data, file, default_flow_style=False, sort_keys=False, indent=4)
         response.success = True
         response.message = f"save parameters to {self.location} "
         return response
@@ -61,9 +84,10 @@ class ParameterManagerEx():
             for item in key_path:
                 source = source[item]
             
-            source[key_to_update] = self.node.get_parameter(key).value
+            try:
+                source[key_to_update] = self.node.get_parameter(key).value
+            except Exception as err:
+                self.node.get_logger().error(f"Failed to update yaml key: {key_to_update} with param name; {key}")
 
-        print("------")
-        print(data)
 
         
