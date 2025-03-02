0f619c64478ec172055f4eb7ad0a29d11af6a5a1
robo2020 <robo2020@gmail.com>
2025-03-02 09:36:12 +0000

    support node naming

--- src/parameters_manager_ex/parameters_manager_ex/__init__.py
+++ src/parameters_manager_ex/parameters_manager_ex/__init__.py
@@ -9,23 +9,39 @@ import os
 TOPIC = "dump"
 
 class ParameterManagerEx():
-    def __init__(self, node: Node):
+    def __init__(self, node: Node, root=""):
         self.node = node
         self._data = None
+        self.root = root
         self.location = self.node.get_parameter("param_yaml_full_path").value
-        self._load_parameters()
+        self._load_parameters(root)
         
         self._init_service()
 
 
-    def _load_parameters(self):
+    def _open_parameters(self, root):
         if not os.path.exists(self.location):
             self.node.get_logger().error(f"Parameter file not found: {self.location}")
             return
         
         with open(self.location, "r") as file:
-            self._data = yaml.safe_load(file)
-        self.append_yaml_to_node(self._data)
+            data = yaml.safe_load(file)
+
+        if root:
+            self.node.get_logger().error(f"root name ::: {root}")
+            if root in data:
+                data = data[root]
+            else:
+                self.node.get_logger().warning(f"root {root} node found, parameters not update from this file" )
+                data = None
+            return data
+            
+    def _load_parameters(self, root):
+        data = self._open_parameters(root)
+        if not data:
+            return
+        
+        self.append_yaml_to_node(data)
 
 
     def _init_service(self):
@@ -58,11 +74,26 @@ class ParameterManagerEx():
         self.traverse_yaml(data, handler=self._create_node_handler)
 
     def save_tracking_parameters(self, request: Trigger.Request, response: Trigger.Response):
-        self.get_tracking_parameters(self._data)
+        #TODO: how to lock between process in python 
+
+        with open(self.location, "r") as file:
+            data = yaml.safe_load(file)
+
+        node_part = None
+        if self.root:
+            if self.root in data:
+                node_part = data[self.root]
+
+        if not node_part:
+            response.success = False
+            response.message = f"root: {self.root} not found for saving"
+            return response
+        
+        self.get_tracking_parameters(node_part)
 
         # TODO: add exception to logger
         with open(self.location, "w") as file:
-            yaml.dump(self._data, file, default_flow_style=False, sort_keys=False, indent=4)
+            yaml.dump(data, file, default_flow_style=False, sort_keys=False, indent=4)
         response.success = True
         response.message = f"save parameters to {self.location} "
         return response
